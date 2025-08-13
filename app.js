cb3fdd0407afc4d6d244cf43e0db94670eb87fd7
Shiva69-prasad <shiva31694@gmail.com>
2025-08-13 09:37:11 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,66 @@
+alert("hello world");
+
+console.log(1234);
+ console.log("shivaprasad");
+  var greeting="hello";
+  console.log(greeting);
+  console.log([1,3,6]);
+   console.log({a:'2',b:'9',c:'20'});
+  console.clear()
+  console.assert(1>2,"it is false")
+
+
+
+ var name="mani";
+ console.log(name);
+
+ name="shiva";
+ console.log(name);
+
+
+ var greeting;
+ greeting="hello this is shiva"
+ console.log(greeting);
+
+
+var $name="shivaprakash"
+ console.log($name);
+
+
+ var firstName="shiva" //camel case
+ console.log(firstName)
+
+
+ var first_name="shiva1" //underscore
+ console.log(first_name);
+
+
+ var FirstName="shiva2" //parscal case
+ console.log(FirstName);
+
+
+  let firstName="shiva" //camel case
+  console.log(firstName)
+
+
+  firstName="shreeki"
+  console.log(firstName)
+
+
+  const  name="mani";
+ console.log(name);
+
+ name="shiva"; //it is not possible in const
+  console.log(name);
+
+  const name={
+     person:"shreeki"
+  }
+
+  name.person="Medan";
+  console.log(name);
+
+
+ const number=[1,2,56,7]
+ number.push(98);
+ console.log(number);
